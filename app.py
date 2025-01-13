0c39a40337a3c7bd3aa65d28bddc68433c1fc481
Gael Franco Garcia <167834489+GaelFG17@users.noreply.github.com>
2025-01-13 01:19:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -147,4 +147,5 @@ def detectar_puntos_y_procesar_imagenes():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Usa el puerto especificado o 5000 por defecto
+    app.run(host='0.0.0.0', port=port)
